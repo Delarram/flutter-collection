622406420405cc8706283e83b3088bad438f68c3
Delarram <tunyanaing960@gmail.com>
2023-03-18 23:59:08 +0630

    added body section

--- lib/pages/filter/choose_category_page.dart
+++ lib/pages/filter/choose_category_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../resources/color_constant.dart';
+import '../../widget/custom_textfield.dart';
 import '../../widget/widget.dart';
 
 class ChooseCategoryPage extends StatelessWidget {
@@ -21,7 +22,62 @@ class ChooseCategoryPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
+            Container(
+              padding:
+              EdgeInsets.symmetric(horizontal: 15, vertical: 7),
+              decoration: const BoxDecoration(
+                  border: Border.symmetric(
+                      horizontal: BorderSide(color: Colors.black26))),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.r),
+                          color: cBackGroundFive),
+                      child: Row(
+                        children: [
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          const Icon(
+                            Icons.search_outlined,
+                            color: cPrimaryThree,
+                          ),
+                          Expanded(
+                            child: CustomTextField(
+                              isSearch: true,
+                              hintText: "Search Tests",
 
+                              fillColor: cBackGroundFive,
+                              onChange: (value) {
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                      onTap: () {
+                        bloc.onChangePress();
+                      },
+                      child: Padding(
+                        padding:
+                        const EdgeInsets.only(left: 10, right: 10),
+                        child: Icon(
+                          bloc.isPress
+                              ? Icons.filter_alt_outlined
+                              : Icons.filter_list_alt,
+                          color: bloc.isPress
+                              ? cBackGroundTwo
+                              : cPrimaryThree,
+                          size: bloc.isPress ? 30 : 35,
+                        ),
+                      ))
+                ],
+              ),
+            ),
           ],
         ),
       ),
