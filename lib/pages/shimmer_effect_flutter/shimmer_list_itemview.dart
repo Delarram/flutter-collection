7c9a90cf966572102ec1784a50b37725542b9781
Delarram <tunyanaing960@gmail.com>
2023-03-29 15:47:19 +0630

    updated itemview

---
+++ lib/pages/shimmer_effect_flutter/shimmer_list_itemview.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+
+class Shimmer_listItem_view extends StatelessWidget {
+  const Shimmer_listItem_view({
+    Key? key,
+    required this.image,
+  }) : super(key: key);
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            SizedBox(
+              height: 120,
+              width: 120,
+              child: Image.asset(image),
+            ),
+            const SizedBox(width: 15),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Euronews",
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15),
+                    child: Text(
+                      "On politics with Lisa Loureniani: Warren’s growing crowds",
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  ),
+                  Row(
+                    children:  [
+                      Text(
+                        "Politics",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 15),
+                        child: CircleAvatar(
+                          radius: 3,
+                          backgroundColor: Colors.grey.shade100,
+                        ),
+                      ),
+                      Text(
+                        "3m ago",
+                        style: TextStyle(color:Colors.grey.shade100),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
