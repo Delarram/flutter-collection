0b402912fd2a3f0b9cded597d1628218fefcb079
Delarram <tunyanaing960@gmail.com>
2023-03-29 16:00:24 +0630

    updated shima

---
+++ lib/pages/shimmer_effect_flutter/skeleton.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Skeleton extends StatelessWidget {
+  const Skeleton({Key? key, required this.height, required this.width}) : super(key: key);
+
+  final double height,width;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        color: Colors.black.withOpacity(0.4),
+        borderRadius: BorderRadius.circular(16)
+      ),
+    );
+  }
+}
